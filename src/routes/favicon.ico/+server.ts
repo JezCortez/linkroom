a11ec2c280b210eb8d9b481f671d0bed74f0d1a7
JezCortez <jezkacortez@addu.edu.ph>
2026-04-28 18:37:32 +0800

    favicon change

--- src/routes/favicon.ico/+server.ts
+++
@@ -1,8 +0,0 @@
-import type { RequestHandler } from '@sveltejs/kit';
-
-export const GET: RequestHandler = () => {
-  return new Response(null, {
-    status: 302,
-    headers: { Location: '/jezzy.svg' }
-  });
-};
